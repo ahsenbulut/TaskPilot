d762512b631638060ed6e1accee78b5cd42ebe90
ahsenbulut <ahsen.bulut16@gmail.com>
2025-06-27 12:47:28 +0300

    Validation ve hata yönetimi eklendi

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import 'reflect-metadata';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ Global ValidationPipe eklendi
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,              // sadece DTO'da tanımlı alanları kabul eder
+      forbidNonWhitelisted: true,  // DTO'da olmayan alanlar gönderilirse hata verir
+      transform: true               // otomatik tip dönüşümü sağlar
+    }),
+  );
+
   await app.listen(3000);
   console.log('🚀 App is running on http://localhost:3000');
 }
